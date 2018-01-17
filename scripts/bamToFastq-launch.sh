fd8ed8067ff3f6a83f4be720f05171cba11c40e4
Scott G. Daniel <scottdaniel25@gmail.com>
2018-01-17 15:57:52 -0700

    more proper names

--- scripts/bamToFastq-launch.sh
+++ scripts/bamToFastq-launch.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 #
-#script that takes the single bam from TCGA
-#1)sorts it by query/read name
-#2)converts it to fastq
-#3)repairs them so you have forward, reverse, and singleton files
-#
+#script to bam to fastq#
+
 
 set -u
 source ./config.sh
@@ -20,7 +17,7 @@ init_dir "$STDOUT_DIR"
 
 export LIST="$PRJ_DIR"/bam_list
 
-find $HARMONIZED -iname "*.bam" | sed "s/^\.\///" | sort > $LIST
+find $OR_DIR $LONG_DIR -iname "*.bam" | sed "s/^\.\///" | sort > $LIST
 
 NUM_FILES=$(lc $LIST)
 
@@ -33,7 +30,7 @@ let i=1
 while (( "$i" <= "$NUM_FILES" )); do
     export FILE_START=$i
     echo Doing file $i plus 9 more if possible
-    JOB=$(qsub -V -N sort-and-prep -j oe -o "$STDOUT_DIR" $WORKER_DIR/sort-and-prep.sh)
+    JOB=$(qsub -V -N sort-and-prep -j oe -o "$STDOUT_DIR" $WORKER_DIR/bamToFastq.sh)
     if [ $? -eq 0 ]; then
       echo "Submitted job \"$JOB\" for you in steps of \"$STEP_SIZE.\""
     else
