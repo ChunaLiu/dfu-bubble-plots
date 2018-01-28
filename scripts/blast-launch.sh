ebd5e9483b1c79e059c06a3aecb38a04850ce9ef
Scott G. Daniel <scottdaniel25@gmail.com>
2018-01-28 11:08:12 -0700

    oh yeah blast

---
+++ scripts/blast-launch.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+#
+# runs blastn
+#
+
+set -u
+source ./config.sh
+export CWD="$PWD"
+#batches of N
+export STEP_SIZE=3
+
+PROG=`basename $0 ".sh"`
+STDOUT_DIR="$CWD/pbs_logs/$PROG"
+
+init_dir "$STDOUT_DIR" 
+
+cd $PRJ_DIR
+
+export FQLIST="$MY_TEMP_DIR"/"fastq_list"
+
+find $TM_OR_DIR $TM_LONG_DIR -iname "*_trimmed.fq" > $FQLIST
+
+mkdir -p $BLAST_OR_DIR
+mkdir -p $BLAST_LONG_DIR
+
+export TODO="$MY_TEMP_DIR"/"files_todo"
+
+if [ -e $TODO ]; then
+    rm $TODO
+fi
+
+echo "Checking if trimming has already been done for dna"
+
+while read FASTQ; do
+
+    if [[ $FASTQ =~ "Long" ]]; then
+
+        if [ ! -e "$BLAST_LONG_DIR/$(basename $FASTQ _trimmed.fq).blast" ]; then
+            echo $FASTQ >> $TODO
+        fi
+
+    else
+
+        if [ ! -e "$BLAST_OR_DIR/$(basename $FASTQ _trimmed.fq).blast" ]; then
+            echo $FASTQ >> $TODO
+        fi
+
+    fi
+
+done < $FQLIST
+
+NUM_FILES=$(lc $TODO)
+
+echo Found \"$NUM_FILES\" files in \"$PRJ_DIR\" to work on
+
+JOB=$(qsub -J 1-$NUM_FILES:$STEP_SIZE -V -N trimgalore -j oe -o "$STDOUT_DIR" $WORKER_DIR/blast.sh)
+
+if [ $? -eq 0 ]; then
+  echo Submitted job \"$JOB\" for you in steps of \"$STEP_SIZE.\" why me worry?.
+else
+  echo -e "\nError submitting job\n$JOB\n"
+fi
+
---
+++ scripts/workers/blast.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+#PBS -W group_list=bhurwitz
+#PBS -q standard
+#PBS -l select=1:ncpus=12:mem=34gb
+#PBS -l walltime=12:00:00
+#PBS -l cput=12:00:00
+#PBS -M scottdaniel@email.arizona.edu
+#PBS -m bea
+
+set -u
+
+CONFIG="$SCRIPT_DIR/config.sh"
+
+if [[ -e $CONFIG ]]; then
+    source $CONFIG
+else
+    echo "no config file"
+    exit 1
+fi
+
+module load blast
+
+echo "Started at $(date) on host $(hostname)"
+
+TMP_FILES=$(mktemp)
+
+get_lines $TODO $TMP_FILES $PBS_ARRAY_INDEX $STEP_SIZE
+
+NUM_FILES=$(lc $TMP_FILES)
+
+echo "Found $NUM_FILES files to process"
+
+while read FASTQ; do
+
+    BASE=$(basename $FASTQ _trimmed.fq)
+
+    echo "Blasting $FASTQ against $BLASTDB"
+
+    if [[ $FASTQ =~ "Long" ]]; then
+
+      fastq-to-fasta.py -o- $FASTQ | blastn -db $BLASTDB \
+          -query - -num_alignments $NUMALNS -perc_identity $IDENT \
+          -qcov_hsp_perc $QCOV -num_threads 12 \
+          -out $BLAST_LONG_DIR/$BASE.blast \
+          -outfmt '6 qaccver saccver pident qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxid'
+
+    else
+
+       fastq-to-fasta.py -o- $FASTQ | blastn -db $BLASTDB \
+          -query - -num_alignments $NUMALNS -perc_identity $IDENT \
+          -qcov_hsp_perc $QCOV -num_threads 12 \
+          -out $BLAST_OR_DIR/$BASE.blast \
+          -outfmt '6 qaccver saccver pident qcovhsp length mismatch gapopen qstart qend sstart send evalue bitscore staxid'
+    
+    fi
+
+    echo "Done with $BASE hopefully that worked"
+
+done < "$TMP_FILES"
+
+echo "Done at $(date)"
